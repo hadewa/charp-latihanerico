217354e1a052db6eca930843a53007f672772658
hadewa <rivaldihandaka@gmail.com>
2018-07-10 11:40:33 +0700

    Add files via upload

---
+++ Bab4.2/Tracking/TrackingNo1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TrackingNo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            string input;
+
+            input = Console.ReadLine();
+            int.TryParse(input, out a);
+            input = Console.ReadLine();
+            int.TryParse(input, out b);
+
+            a = a+b;
+            b = b+a;
+
+            Console.Write("{0} {1}", a, b);
+        }
+    }
+}
+/* Bab 4 - Tracking nomor 1
+Soal    :   Tentukanlah apa yang ditampilkan ke layar oleh kode
+            program di bawah ini jika input yang diberikan adalah 2 dan
+            3.
+
+Hasil   :   5 8 
+*/
---
+++ Bab4.2/Tracking/TrackingNo2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TrackingNo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string input;
+
+            input = Console.ReadLine();
+            int.TryParse(input, out a);
+            input = Console.ReadLine();
+            int.TryParse(input, out b);
+
+            c = a++;
+            a = --b;
+            b = c++;
+
+            Console.Write("{0} {1} {2}", a, b, c);
+        }
+    }
+}
+/* Bab 4 - Tracking nomor 2
+Soal    :   Tentukanlah apa yang ditampilkan ke layar oleh kode
+            program di bawah ini jika input yang diberikan adalah 2 dan
+            3.
+            
+Hasil   :   2 2 3 */
